572d800b90c762dd6447f07f39fa43f16b758d8e
ASchopenhaur <alexhernandez800@hotmail.com>
2017-02-14 19:15:39 -0600

    initial commit

---
+++ routes/tasks.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://<dbuser>:<dbpassword>@ds149069.mlab.com:49069/mytasklist_as', ['tasks']);
+
+// Get All Tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(tasks);
+        }
+    });
+});
+
+// Get Single Task
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
+});
+
+// Save Task
+router.post('/task', function(req, res, next){
+    var task = req.body;
+    if(!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else {
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }else {
+                res.json(task);
+            }
+        });
+    }
+});
+
+// Delete Task
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
+});
+
+// Update Task
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updTask = {};
+
+    if(task.isDone){
+        updTask.isDone = task.isDone;
+    }
+
+    if(task.title){
+        updTask.title = task.title;
+    }
+
+    if(!updTask){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updTask, {}, function(err, task){
+        if(err){
+            res.send(err);
+        }else {
+            res.json(task);
+        }
+    });
+    }
+});
+
+module.exports = router;
